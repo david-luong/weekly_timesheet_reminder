e5ec8e93fbbe48966cd30578f542994950d662db
David Luong <64257209+david-luong@users.noreply.github.com>
2021-08-19 21:10:52 +1000

    Update Weekly_Reminder.py

--- Weekly_Reminder.py
+++ Weekly_Reminder.py
@@ -1,7 +1,3 @@
-##Randomise words for weekly timesheet reminder to avoid overthinking.
-##Because automated timesheet reminders aren't very effective.
-#need to have random and emoji packages installed.
-
 #to enable use of shuffling via random.shuffle to mix up the lists..
 import random
 
@@ -9,25 +5,24 @@ import random
 #https://pypi.org/project/emoji/ (pip install emoji --upgrade)
 import emoji
 
-#typical phrase structure
-#{STARTS} {NOUN}, weekly reminder to submit your timesheets! {EMOJI}
+#typical phrase structure "{STARTS} {NOUN}, weekly reminder to submit your timesheets! {EMOJI}"
 
 emojis = [':smile:',':blush:',':laughing:',':smiley:',':satisfied:',':heart_eyes:',':grinning:',':innocent:',':grin:']
 nouns = ['team','everyone','everybody','all']
 starts = ['hi','hello','hey']
 
-#to test emojis are compatible
+#[optional code] for testing different emojis
 #for emote in emojis:
 #    print(emoji.emojize(emote,use_aliases=True))
 
-#shuffle the respective lists
+#shuffle each respective list
 random.shuffle(emojis)
 random.shuffle(nouns)
 random.shuffle(starts)
 
 random_string = "{} {}, weekly reminder to submit your timesheets! {}".format(starts[0],nouns[0],emojis[0])
 
-#enable use_aliases=True for full dictionary of emojis
+#inclusion of use_aliases=True for full dictionary of emojis
 string_with_emojis = emoji.emojize(random_string,use_aliases=True)
 bold_string = "\033[1m"+string_with_emojis+"\033[0m"
 print(bold_string)
